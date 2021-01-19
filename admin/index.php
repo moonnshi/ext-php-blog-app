d0c3123a0ec83d609b7224c24b2e6b6861a01188
shoonlaemay <shoonlaemay>
2021-01-19 13:44:12 +0630

    ref:format code and cmt added in index.php

--- admin/index.php
+++ admin/index.php
@@ -1,19 +1,36 @@
 <?php
 require '../config/config.php';
+
   session_start();
+
+  // if no login sesion, back to login
   if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])){
     header('Location:login.php');
   }
+
+  // if not admin role, back to login
+  if($_SESSION['role']!=1){
+    header('Location:login.php');
+  }
+
+  if(!empty($_POST['search'])){
+    setcookie("search", $_POST['search'], time() + (86400 * 30), "/"); // set search key to cookie
+  }
+  else{
+    if(empty($_GET['pageno'])){
+      unset($_COOKIE['search']); // remove search key from cookie
+      setcookie('search', null, -1, '/');
+    }
+  }
 ?>
 
-<?php include('header.html')?>
+<?php include('header.php')?>
+
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
       <!-- Content Header (Page header) -->
-      <div class="content-header">
-      </div>
+      <div class="content-header"></div>
       <!-- /.content-header -->
-
       <!-- Main content -->
       <div class="content">
         <div class="col-md-12">
@@ -23,12 +40,11 @@ require '../config/config.php';
             </div>
             <!-- /.card-header -->
             <?php
+              $pageno=(!empty($_GET['pageno']))?$_GET['pageno']:'1'; // get pageno from request param
+              $numOfRec=3;
+              $offset=($pageno-1)*$numOfRec; // get offeset from pagesize and pageno
 
-              $pageno=(!empty($_GET['pageno']))?$_GET['pageno']:'1';
-              $numOfRec=10;
-              $offset=($pageno-1)*$numOfRec;
-
-              if(empty($_POST['search'])){
+              if(empty($_POST['search']) && (empty($_COOKIE['search']))){ // if no search key, retrieve all blog posts
                 $statement=$pdo->prepare('SELECT * from posts ORDER BY id DESC ');
                 $statement->execute();
                 $rawResults=$statement->fetchAll();
@@ -39,8 +55,8 @@ require '../config/config.php';
                 $results=$statement->fetchAll();
 
               }
-              else{
-                $search=$_POST['search'];
+              else{ // retrieve blog posts by search key
+                $search=(!empty($_POST['search']))? $_POST['search'] : $_COOKIE['search'];
                 $statement=$pdo->prepare("SELECT * from posts WHERE title LIKE '%$search%' ORDER BY id DESC ");
                 $statement->execute();
                 $rawResults=$statement->fetchAll();
@@ -49,9 +65,7 @@ require '../config/config.php';
                 $statement=$pdo->prepare("SELECT * from posts WHERE title LIKE '%$search%' ORDER BY id DESC LIMIT $offset,$numOfRec");
                 $statement->execute();
                 $results=$statement->fetchAll();
-                
               }
-
             ?>
             <div class="card-body">
               <div>
@@ -70,7 +84,7 @@ require '../config/config.php';
                   <?php if($results):?>
                     <?php $i=1; foreach ($results as $post):?>
                       <tr>
-                        <td><?= $offset+1 ?></td>
+                        <td><?= $offset+$i ?></td>
                         <td><?= $post['title']?></td>
                         <td><?= substr($post['content'],0,100)?></td>
                         <td>
